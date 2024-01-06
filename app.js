3594651dbf8396a78d10282ecdf745132eeee8f4
Hakimsabkhi <hakimsabkhi001@gmail.com>
2024-01-06 09:27:41 +0100

    commit second

--- app.js
+++ app.js
@@ -1,11 +1,57 @@
-const express = require ('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+const mongoose = require("mongoose");
+const Mydata = require("./models/dataSchema");
 
-app.get('/', (req, res) => {
-  res.sendFile("./views/home.html", {root:__dirname})
-})
+///auto refresh////////////////
+const path = require("path");
+const livereload = require("livereload");
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, "public"));
 
-app.listen(port, () => {
-  console.log (`http://127.0.0.1:${port}/`)
-})
+const connectLivereload = require("connect-livereload");
+app.use(connectLivereload());
+
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+//////////////////////////////
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index", {});
+});
+
+app.get("/user/add.html", (req, res) => {
+  res.render("user/add");
+});
+
+app.get("/user/view.html", (req, res) => {
+  res.render("user/view");
+});
+
+app.get("/user/edit.html", (req, res) => {
+  res.render("user/edit");
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://hakim:3hN9Y70U6XCTwJqt@cluster0.pnc7mzh.mongodb.net/all-data?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`http://127.0.0.1:${port}/`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
